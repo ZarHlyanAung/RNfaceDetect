26063223e7b0675a367098286cf49eb88e4bfca4
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-03-20 20:12:26 +0630

    ready to test face detection if fix expo img picker

--- App.tsx
+++ App.tsx
@@ -1,118 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import FaceDetection, {Face} from '@react-native-ml-kit/face-detection';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import FaceMap from './app/FaceMap';
+import ChooseImageButton, {ImageDetails} from './app/core/ChooseImageButton';
+import OptionSwitch from './app/core/OptionSwitch';
+import PreviewImage from './app/core/PreviewImage';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const FaceDetectionScreen = () => {
+  const [image, setImage] = useState<ImageDetails>();
+  const [faces, setFaces] = useState<Face[]>([]);
+  const [showFrame, setShowFrame] = useState(true);
+  const [showLandmarks, setShowLandmarks] = useState(false);
+  const [showContours, setShowContours] = useState(false);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleChoose = async (currentImage: ImageDetails) => {
+    setImage(currentImage);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    const result = await FaceDetection.detect(currentImage.path, {
+      landmarkMode: 'all',
+      contourMode: 'all',
+    });
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    setFaces(result);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.container}>
+      <ChooseImageButton onChoose={handleChoose} />
+
+      {image && (
+        <View style={styles.imageContainer}>
+          <PreviewImage source={image.path} />
+
+          {faces.map(face => (
+            <FaceMap
+              key={Math.random()}
+              face={face}
+              width={image.width}
+              height={image.height}
+              showFrame={showFrame}
+              showContours={showContours}
+              showLandmarks={showLandmarks}
+            />
+          ))}
+
+          <OptionSwitch
+            label="Show Frame"
+            value={showFrame}
+            onChange={setShowFrame}
+          />
+          <OptionSwitch
+            label="Show Landmarks"
+            value={showLandmarks}
+            onChange={setShowLandmarks}
+          />
+          <OptionSwitch
+            label="Show Contours"
+            value={showContours}
+            onChange={setShowContours}
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )}
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  imageContainer: {
+    marginTop: 15,
+    marginBottom: 20,
   },
 });
 
-export default App;
+export default FaceDetectionScreen;
